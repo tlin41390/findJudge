753cab31ae75bc954d4936169f6b9eec150318d4
tlin41390 <tlin41@dons.usfca.edu>
2020-11-11 01:12:14 -0800

    findJudge

--- Judge.java
+++ Judge.java
@@ -1,23 +1,29 @@
 import java.util.*;
 public class Judge
 {
-   public int findJudge(int N, int[][]townsFolk)
-   {
-       int [] trustArray = new int[N];
+   public int findJudge(int total, int[][]townsFolk)
+   {   // initializes the number of people trusting the judge.
+       int [] trustArray = new int[total+1];
 
        for(int i =0;i<townsFolk.length;i++)
        {
            int[] item = townsFolk[i];
-           trustArray[item[0]--]--;
-           trustArray[item[1]--]++;
+           trustArray[item[0]]--;
+           trustArray[item[1]]++;
        }
-       for(int i = 0;i<N;i++)
+       for(int i = 1;i<=total;i++)
        {
-           if(trustArray[i]==N-1)
+           if(trustArray[i]==total-1)
            {
                return i;
            }
        }
        return -1;
+   }
+   public static void main(String[] args)
+   {
+       int[][] townsFolk = {{1,3},{1,4},{2,3},{2,4},{4,3}};
+       Judge finder = new Judge();
+       System.out.println("Result: "+finder.findJudge(4,townsFolk));
    }
 }
