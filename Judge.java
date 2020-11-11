abe6a14355e7e174d3fd664053e2a9f5b74bd595
tlin41390 <tlin41@dons.usfca.edu>
2020-11-11 01:00:53 -0800

    finished the findJudge function

--- Judge.java
+++ Judge.java
@@ -1,1 +1,23 @@
+import java.util.*;
+public class Judge
+{
+   public int findJudge(int N, int[][]townsFolk)
+   {
+       int [] trustArray = new int[N];
 
+       for(int i =0;i<townsFolk.length;i++)
+       {
+           int[] item = townsFolk[i];
+           trustArray[item[0]--]--;
+           trustArray[item[1]--]++;
+       }
+       for(int i = 0;i<N;i++)
+       {
+           if(trustArray[i]==N-1)
+           {
+               return i;
+           }
+       }
+       return -1;
+   }
+}
